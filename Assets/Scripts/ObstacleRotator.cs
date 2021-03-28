a3fc70c0c957849162592cd720d07a5669f48979
Feraxion <32463690+Feraxion@users.noreply.github.com>
2021-03-29 00:31:18 +0300

    Basic Obstacle and Test Scene
    Obstacle tag and rotating obstacles

---
+++ Assets/Scripts/ObstacleRotator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleRotator : MonoBehaviour
+{
+    private Transform GO;
+
+    [SerializeField]
+    private int RotateSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        //GO = gameObject.transform;
+        //GO.transform.rotation.x  += 5f;
+        
+        gameObject.transform.Rotate(RotateSpeed,0f,0f);
+    }
+}
